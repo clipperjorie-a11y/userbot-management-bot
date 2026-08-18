f3f10087a34195b54614677d18ed5be19c6bbde7
clipperjorie-a11y <clipperjorie@gmail.com>
2026-08-18 16:36:32 +0700

    main.py

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 import asyncio
 from bot import app as bot_app
-from userbot import app as ubot_app, timer_task
+from userbot import timer_task
 
 async def main():
-    print("🚀 Running Bot Controller & Engine Userbot (Tombol Toggle Mode)...")
+    print("🚀 Running Bot Controller & Engine Userbot (Multi-Client OTP Mode)...")
     await asyncio.gather(
         bot_app.start(),
-        ubot_app.start(),
         timer_task()
     )
     await asyncio.Event().wait()
 
 if __name__ == "__main__":
     asyncio.run(main())
-  
+    
